8a5c540165612f68fdfde45eb03522463726da21
Bruce MacKinnon <brucemackinnon@Family-iMac.local>
2021-12-23 09:49:20 -0500

    Updates

--- Christmas2021.ino
+++ Christmas2021.ino
@@ -1,7 +1,16 @@
+// Christmas Light Controller
+// 2021 Display
+//
+// Hardware is an Arduio Pro Mini, two 74HCT595 shift registers,
+// and 16 solid-state relays. 
+//
+// GPIO pins for shift register.
+// CHANGE THESE ACCORDING TO YOUR HARDWARE SETUP!
 #define SRCLK 8
-#define RCLK 7
-#define SER 9
+#define RCLK  7
+#define SER   9
 
+// String addresses
 #define STAR_W1    0x0001
 #define STAR_W2    0x0002
 #define STAR_W3    0x0004
@@ -16,6 +25,8 @@
 #define LEFT_BUSH  0x0800
 #define RIGHT_BUSH 0x1000
 #define WREATH     0x2000
+#define RIGHT_MC   0x4000
+#define LEFT_MC    0x8000
 
 void setup() {
   
@@ -28,8 +39,9 @@ void setup() {
 
   int d0 = 1000;
 
-  // Diagnostics
+  // Diagnostics on startup
   write(0);
+  // Flash on-board LED
   digitalWrite(13,1);
   delay(d0);
   digitalWrite(13,0);
@@ -39,24 +51,29 @@ void setup() {
   digitalWrite(13,0);
   delay(d0);
   delay(d0);
+  // Turn on every string
   write(0xffff);
   delay(d0);
   delay(d0);
   delay(d0);
+  // Turn off every string
   write(0);
 }
 
+// Single right-shift on shift register
 void shift() {
   digitalWrite(SRCLK,1);
   delay(10);
   digitalWrite(SRCLK,0);
 }
 
+// Latch output for display
 void latch() {
   digitalWrite(RCLK,1);
   digitalWrite(RCLK,0);
 }
 
+// Writes a 16-bit value into the register and latches
 void write(unsigned int data) {
   for (int i = 0; i < 16; i++) {
     if (data & 1) {
@@ -72,6 +89,7 @@ void write(unsigned int data) {
 
 void showCycle() {
 
+  int df = 75;
   int d0 = 125;
   int d1 = 250;
   int d2 = 500;
@@ -80,55 +98,123 @@ void showCycle() {
   delay(d1);
 
   // Long white
-  write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5 | STAR_WC);
+  write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5 | STAR_WC | WREATH);
   delay(d2);
   // Long red
   write(STAR_R1 | STAR_R2 | STAR_R3 | STAR_R4 | STAR_R5);
   delay(d2);
 
   // Sweep bush/wreath
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 2; i++) {
+    write(LEFT_MC);
+    delay(df);
     write(LEFT_BUSH);
-    delay(d0);
+    delay(df);
     write(RIGHT_BUSH);
-    delay(d0);
+    delay(df);
     write(WREATH);
-    delay(d0);
+    delay(df);
+    write(RIGHT_MC);
+    delay(df);
+    write(WREATH);
+    delay(df);
+    write(RIGHT_BUSH);
+    delay(df);
+    write(LEFT_BUSH);
+    delay(df);
   }
     
   // Star rotation
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 2; i++) {
+    
     int wc = 0;
-    if (i % 2 == 0) {
-      wc = STAR_WC;
-    } else {
-      wc = LEFT_BUSH | RIGHT_BUSH | WREATH;
-    }
-    write(STAR_W1 | STAR_R1 | wc);
-    delay(d0);
-    write(STAR_W2 | STAR_R5 | wc);
-    delay(d0);
-    write(STAR_W3 | STAR_R4 | wc);
-    delay(d0);
-    write(STAR_W4 | STAR_R3 | wc);
-    delay(d0);
-    write(STAR_W5 | STAR_R2 | wc);
-    delay(d0);
+    wc = STAR_WC | LEFT_BUSH | RIGHT_BUSH | WREATH;
+
+    // Red background
+    wc = STAR_WC | LEFT_BUSH | RIGHT_BUSH | WREATH;
+    wc |= (STAR_R1 | STAR_R2 | STAR_R3 | STAR_R4 | STAR_R5);
+  
+    write(STAR_W1 | wc);
+    delay(df);
+    write(STAR_W2 | wc);
+    delay(df);
+    write(STAR_W3 | wc);
+    delay(df);
+    write(STAR_W4 | wc);
+    delay(df);
+    write(STAR_W5 | wc);
+    delay(df);
+
+    // White background
+    wc = STAR_WC | LEFT_BUSH | RIGHT_BUSH | WREATH;
+    wc |= (STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5);
+
+    write(STAR_R1 | wc);
+    delay(df);
+    write(STAR_R2 | wc);
+    delay(df);
+    write(STAR_R3 | wc);
+    delay(df);
+    write(STAR_R4 | wc);
+    delay(df);
+    write(STAR_R5 | wc);
+    delay(df);
   }
 
+  // Star build
   for (int i = 0; i < 2; i++) {
-    write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5 | STAR_WC);
+    
+    int wc = 0;
+    wc = STAR_WC | LEFT_BUSH | RIGHT_BUSH | WREATH;
+
+    // White
+    write(STAR_W1 | wc);
+    delay(df);
+    write(STAR_W1 | STAR_W2 | wc);
+    delay(df);
+    write(STAR_W1 | STAR_W2 | STAR_W3 | wc);
+    delay(df);
+    write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | wc);
+    delay(df);
+    write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5 | wc);
+    delay(df);
+    
+    // Red
+    wc = LEFT_MC | RIGHT_MC;
+    write(STAR_R1 | wc);
+    delay(df);
+    write(STAR_R1 | STAR_R2 | wc);
+    delay(df);
+    write(STAR_R1 | STAR_R2 | STAR_R3 | wc);
+    delay(df);
+    write(STAR_R1 | STAR_R2 | STAR_R3 | STAR_R4 | wc);
+    delay(df);
+    write(STAR_R1 | STAR_R2 | STAR_R3 | STAR_R4 | STAR_R5 | wc);
+    delay(df);
+  }
+
+  // Red/white alt
+  for (int i = 0; i < 2; i++) {
+    write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5 | STAR_WC | WREATH);
     delay(d0);
     write(STAR_R1 | STAR_R2 | STAR_R3 | STAR_R4 | STAR_R5);
     delay(d0);  
-    write(LEFT_BUSH | RIGHT_BUSH);
+    write(LEFT_BUSH | RIGHT_BUSH | LEFT_MC | RIGHT_MC);
     delay(d0);  
   }
 
-  // All
+  // All white
+  write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5 | STAR_WC |
+      WREATH);
+  delay(d2);
+  delay(d2);
+  
+  // All 
   write(STAR_W1 | STAR_W2 | STAR_W3 | STAR_W4 | STAR_W5 | STAR_WC |
       STAR_R1 | STAR_R2 | STAR_R3 | STAR_R4 | STAR_R5 |
-      LEFT_BUSH | RIGHT_BUSH | WREATH);
+      LEFT_BUSH | RIGHT_BUSH | WREATH |
+      LEFT_MC | RIGHT_MC);
+  delay(d2);
   delay(d2);
   delay(d2);
 }
@@ -136,7 +222,4 @@ void showCycle() {
 void loop() {
   showCycle();
 }
-
-
-
 
